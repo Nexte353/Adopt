7fe8e8ee25347c18d2524ff9129a651daffc3bd5
Nexte353 <123237229+Nexte353@users.noreply.github.com>
2024-11-30 23:21:10 +0700

    Update AdoptStart.lua

--- AdoptStart.lua
+++ AdoptStart.lua
@@ -1,6 +1,7 @@
 local APIs = game.ReplicatedStorage.API:GetChildren()
 
 for num, i in APIs do
-    i:FireServer()
+    local succes, Error = pcall(function (...)
+           i:FireServer() 
+    end)
 end
---dsdsdsdsd
